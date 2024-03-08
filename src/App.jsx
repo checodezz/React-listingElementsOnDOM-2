6a5f080a2f71bb159882be6d16dd8b712b73f5ba
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-03-08 07:20:25 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+import "./App.css";
+
+const Cars = ({ cars }) => {
+  const carsList = cars.map((car) => (
+    <div key={car.id}>
+      <h3>Brand: {car.brand}</h3>
+      <p>Price: ${car.price}</p>
+    </div>
+  ));
+
+  return (
+    <>
+      <h2>Cars</h2>
+      {carsList}
+      <hr />
+    </>
+  );
+};
+
+const Hotels = ({ hotels }) => {
+  const hotelList = hotels.map((hotel) => (
+    <div key={hotel.id}>
+      <h3>Name: {hotel.name}</h3>
+      <p>Location: {hotel.location}</p>
+    </div>
+  ));
+
+  return (
+    <>
+      <h2>Hotels</h2>
+      {hotelList}
+      <hr />
+    </>
+  );
+};
+
+const Podcasts = ({ podcasts }) => {
+  const podcastList = podcasts.map((podcast) => (
+    <div key={podcast.id}>
+      <h3>Title: {podcast.title}</h3>
+      <p>Host: {podcast.host}</p>
+    </div>
+  ));
+
+  return (
+    <>
+      <h2>Podcasts</h2>
+      {podcastList}
+      <hr />
+    </>
+  );
+};
+
+const Articles = ({ articles }) => {
+  const articleList = articles.map((article) => (
+    <div key={article.id}>
+      <h3>Title: {article.title}</h3>
+      <p>{article.content}</p>
+    </div>
+  ));
+
+  return (
+    <>
+      <h2>Articles</h2>
+      {articleList}
+      <hr/>
+    </>
+  );
+};
+
+const Movies = ({ movies }) => {
+  const movieList = movies.map((movie) => (
+    <div key={movie.id}>
+      <h3>Title: {movie.title}</h3>
+      <p>Director: {movie.director}</p>
+    </div>
+  ));
+
+  return (
+    <>
+      <h2>Movies</h2>
+      {movieList}
+    </>
+  );
+};
+export default function App() {
+  const cars = [
+    { id: 1, brand: "Toyota", price: 20000 },
+    { id: 2, brand: "Honda", price: 25000 },
+    { id: 3, brand: "Ford", price: 30000 },
+  ];
+  const hotels = [
+    { id: 1, name: "Marriott", location: "New York" },
+    { id: 2, name: "Hilton", location: "Los Angeles" },
+    { id: 3, name: "Sheraton", location: "Chicago" },
+  ];
+
+  const podcasts = [
+    { id: 1, title: "Podcast 1", host: "Host 1" },
+    { id: 2, title: "Podcast 2", host: "Host 2" },
+    { id: 3, title: "Podcast 3", host: "Host 3" },
+  ];
+
+  const articles = [
+    { id: 1, title: "Article 1", content: "Content 1" },
+    { id: 2, title: "Article 2", content: "Content 2" },
+    { id: 3, title: "Article 3", content: "Content 3" },
+  ];
+
+  const movies = [
+    { id: 1, title: "Movie 1", director: "Director 1" },
+    { id: 2, title: "Movie 2", director: "Director 2" },
+    { id: 3, title: "Movie 3", director: "Director 3" },
+  ];
+
+  return (
+    <main>
+      <Cars cars={cars} />
+      <Hotels hotels={hotels} />
+      <Podcasts podcasts={podcasts} />
+      <Articles articles={articles} />
+      <Movies movies={movies} />
+    </main>
+  );
+}
